a1ffa0b94d2b8873b6895b5229b696e19b9e6e17
Chaos <tinywonk@yahoo.com>
2022-12-12 00:45:45 -0500

    Hero fold update

---
+++ script.js
@@ -0,0 +1,30 @@
+var effects = {
+    fire : {
+      fire : 0.5,
+      grass : 2,
+      water : 0.5,
+      electric : 1
+    },
+    grass : {
+      fire : 0.5,
+      grass : 0.5,
+      water : 2,
+      electric : 1
+    },
+    water : {
+      fire : 2,
+      grass : 0.5,
+      water : 0.5,
+      electric : 0.5
+    },
+    electric : {
+      fire : 1,
+      grass : 1,
+      water : 2,
+      electric : 0.5
+    }
+  };
+  
+  function calculateDamage(yourType, opponentType, attack, defense){
+    return Math.ceil(50 * (attack/defense) * effects[yourType][opponentType]);
+  }
